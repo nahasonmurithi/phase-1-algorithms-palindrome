62869074c332234ba54577acfde304c7bbb36d11
Nahason <nahason.murithi@student.moringaschool.com>
2023-10-09 22:17:40 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 function isPalindrome(word) {
+  // reverse the input string
+  const reversedWord = reverseString(word);
+  // compare the reversed string to the input
+  return word === reversedWord;
   // Write your algorithm here
 }
-
+function reverseString(word) {
+  return word.split("").reverse().join("");
+}
 /* 
   Add your pseudocode here
 */
 
 /*
   Add written explanation of your solution here
+  the second function reverse the word and compares 
+  it to the first function. this works well since 
+  both are strings
 */
 
 // You can run `node index.js` to view these console logs
